5c894b383e8655531d9b99c82cc27bfca52a9bbb
Uday <iamuday1618@gmail.com>
2025-05-01 20:30:37 +0530

    Add black formatter

--- data/data_downloader.py
+++ data/data_downloader.py
@@ -5,7 +5,9 @@ import pandas as pd
 from astroquery.mast import MastMissions, MastMissionsClass
 
 CONSIDERED_SECTORS = range(1, 27)
-EXOFOP_TOI_URL = "https://exofop.ipac.caltech.edu/tess/download_toi.php?sort=toi?&output=csv"
+EXOFOP_TOI_URL = (
+    "https://exofop.ipac.caltech.edu/tess/download_toi.php?sort=toi?&output=csv"
+)
 Sector = str
 TOI_ID = str
 
@@ -37,7 +39,7 @@ def filter_positive_toi_df(toi_df: pd.DataFrame) -> pd.DataFrame:
     :param toi_df: TOIs list as a pandas.DataFrame
     :return: TOIs list as a pandas.DataFrame
     """
-    return toi_df[toi_df['TFOPWG Disposition'].isin(['CP', 'KP'])]
+    return toi_df[toi_df["TFOPWG Disposition"].isin(["CP", "KP"])]
 
 
 def get_tic_to_sectors(toi_df: pd.DataFrame) -> Mapping[TOI_ID, Iterable[Sector]]:
@@ -48,8 +50,10 @@ def get_tic_to_sectors(toi_df: pd.DataFrame) -> Mapping[TOI_ID, Iterable[Sector]
     :return: The aforementioned dictionary
     """
     tic_dict = {}
-    for tic, sectors in zip(toi_df['TIC ID'], toi_df['Sectors']):
-        sectors = list(filter(lambda sector: int(sector) in CONSIDERED_SECTORS, sectors.split(',')))
+    for tic, sectors in zip(toi_df["TIC ID"], toi_df["Sectors"]):
+        sectors = list(
+            filter(lambda sector: int(sector) in CONSIDERED_SECTORS, sectors.split(","))
+        )
         if sectors:
             tic_dict[tic] = sectors
 
@@ -64,15 +68,17 @@ def generate_uri(tic: str, sector: str) -> str:
     :param sector: The sector of the TIC
     :return: The URI
     """
-    tic = tic.rjust(16, '0')
-    sector = 's' + sector.rjust(4, '0')
-    target = '/'.join(tic[i:i + 4] for i in range(0, len(tic), 4))
+    tic = tic.rjust(16, "0")
+    sector = "s" + sector.rjust(4, "0")
+    target = "/".join(tic[i : i + 4] for i in range(0, len(tic), 4))
 
     uri = f"mast:HLSP/tess-spoc/{sector}/target/{target}/hlsp_tess-spoc_tess_phot_{tic}-{sector}_tess_v1_lc.fits"
     return uri
 
 
-def download_fits_of_tic(mission: MastMissionsClass, tic: str, sector: str, path: str) -> None:
+def download_fits_of_tic(
+    mission: MastMissionsClass, tic: str, sector: str, path: str
+) -> None:
     """
     Download the lightcurve FITS files for a given TIC ID and a sector.
 
@@ -94,7 +100,7 @@ def download_fits(tic_to_sectors: Mapping[str, Iterable[str]], path: str) -> Non
     :param path: The path to save the FITS files to
     :return:
     """
-    mission = MastMissions(mission='tess')
+    mission = MastMissions(mission="tess")
 
     for tic, sectors in tic_to_sectors.items():
         for sector in sectors:
